f0d5fdaa99a93ea73381744bbd25b4285c07bd7a
Naveenreigns1505 <cnaveenreigns@gmail.com>
2023-11-06 17:57:35 +0530

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-var modal = document.getElementById("myModal");
+const form = document.querySelector(".form");
 
 function openForm() {
-  modal.style.display = "block";
+  document.getElementById("registration-form-container").style.display =
+    "block";
+  setTimeout(() => {
+    formContainer.classList.add("active");
+  }, 50);
 }
 
 function closeForm() {
-  modal.style.display = "none";
+  document.getElementById("registration-form-container").style.display = "none";
+  setTimeout(() => {
+    formContainer.style.display = "none";
+  }, 2000);
 }
 
-document
-  .getElementById("registrationForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var email = document.getElementById("email").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
-    console.log("Form submitted:", {
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-    });
-    document.getElementById("registrationForm").reset();
-  });
+const buttons = document.querySelectorAll(".open-form-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", openForm);
+});
+
+function validateForm() {
+  var email = document.getElementById("email").value;
+  var error = document.getElementById("error-msg");
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@yourdomain.com$/;
+  if (!emailRegex.test(email)) {
+    error.textContent =
+      "Please enter your business email address. This form only accepts email addresses from yourdomain.com.";
+    return false;
+  }
+  return true;
+}
+
+function displaySuccessMessage() {
+  const successMsg = document.getElementById("success-msg");
+  successMsg.style.display = "block";
+  setTimeout(function () {
+    successMsg.style.display = "none";
+  }, 2000);
+}
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  displaySuccessMessage();
+});
